62726f8855f7f05c5fcecd6363327131269f9cdb
Admin <trac-andrej@ya.ru>
2020-03-04 09:18:46 +0300

    Add project files.

---
+++ MyCalculator.BL.Tests/MyCalculatorTests.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyCalculator.BL.Tests
+{
+    [TestClass]
+    public class MyCalculatorTests
+    {
+        [TestMethod]
+        public void Summ_10and20_30returned()
+        {
+            // arrange
+            double left = 10;
+            double right = 20;
+            double expected = left + right;
+            double epsilon = 0.00000001;
+
+            // act
+            MyCalculator calculator = new MyCalculator();
+            double actual = calculator.Summ(left, right);
+
+            // assert
+            if (Math.Abs(expected - actual) < epsilon)
+            {
+                Assert.AreEqual(0, 0);
+            }
+            else
+            {
+                Assert.AreEqual(0, 1);
+            }
+            
+        
+        }
+    }
+}
---
+++ MyCalculator.BL/MyCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCalculator.BL
+{
+    public class MyCalculator
+    {
+        public double Summ(double left, double right)
+        {
+            return left + right;
+        }
+    }
+}

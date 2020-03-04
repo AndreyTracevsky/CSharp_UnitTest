248281c46921f95881eda2d391848518599d4a27
Admin <trac-andrej@ya.ru>
2020-03-04 09:29:27 +0300

    Закоментил все строки тела метода. Попробую реализовать метод Summ_10and20_30returned через [DATAROW]

--- MyCalculator.BL.Tests/MyCalculatorTests.cs
+++ MyCalculator.BL.Tests/MyCalculatorTests.cs
@@ -10,24 +10,24 @@ namespace MyCalculator.BL.Tests
         public void Summ_10and20_30returned()
         {
             // arrange
-            double left = 10;
-            double right = 20;
-            double expected = left + right;
-            double epsilon = 0.00000001;
+            //double left = 10;
+            //double right = 20;
+            //double expected = left + right;
+            //double epsilon = 0.00000001;
 
             // act
-            MyCalculator calculator = new MyCalculator();
-            double actual = calculator.Summ(left, right);
+            //MyCalculator calculator = new MyCalculator();
+            //double actual = calculator.Summ(left, right);
 
             // assert
-            if (Math.Abs(expected - actual) < epsilon)
-            {
-                Assert.AreEqual(0, 0);
-            }
-            else
-            {
-                Assert.AreEqual(0, 1);
-            }
+            //if (Math.Abs(expected - actual) < epsilon)
+            //{
+            //    Assert.AreEqual(0, 0);
+            //}
+            //else
+            //{
+            //    Assert.AreEqual(0, 1);
+            //}
             
         
         }

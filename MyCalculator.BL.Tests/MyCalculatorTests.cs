8ef6a8129585ffa19722d813ee086f5fbf237451
Admin <trac-andrej@ya.ru>
2020-03-04 15:19:34 +0300

    Поправил код, вроде все работает. Надо разобраться в каждой строчке и оставить коментарии, чтобы в следующий раз было легче.

--- MyCalculator.BL.Tests/MyCalculatorTests.cs
+++ MyCalculator.BL.Tests/MyCalculatorTests.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace MyCalculator.BL.Tests
@@ -7,41 +7,68 @@ namespace MyCalculator.BL.Tests
     public class MyCalculatorTests
     {
         [DataTestMethod]
-        [DataRow(-1, 3, 2)]
+        // [DataRow(double.MaxValue, double.NaN, double.MaxValue)]
         [DataRow(-1, 11, 10)]
         [DataRow(-1, -11, -12)]
         [DataRow(-10.9, 3.4, -7.5)]
         [DataRow(-10.9, -3.4, -14.3)]
         [DataRow(10.9, 3.4, 14.3)]
+        [DataRow(10.9, 4, 14.9)]
 
 
         public void SummWithDifrentValue(double left, double right, double expected)
         {
-            var calculator = new MyCalculator();
+            var calculator = new MyCalculator(8);
             var actual = calculator.Summ(left, right);
             Assert.AreEqual(expected, actual);
+        }
 
-            // arrange
-            //double left = 10;
-            //double right = 20;
-            //double expected = left + right;
-            //double epsilon = 0.00000001;
+        [DataTestMethod]
+        [DataRow(5, 3, 2)]
+        [DataRow(-5, 3, -8)]
+        [DataRow(-10.9, 3.4, -14.3)]
+        [DataRow(-10.9, -3.4, -7.5)]
+        [DataRow(-10, -3, -7)]
 
-            // act
-            //MyCalculator calculator = new MyCalculator();
-            //double actual = calculator.Summ(left, right);
+        public void MinusWithDifrentValue(double left, double right, double expected)
+        {
+            var calculator = new MyCalculator(8);
+            var actual = calculator.Minus(left, right);
+            Assert.AreEqual(expected, actual);
+        }
 
-            // assert
-            //if (Math.Abs(expected - actual) < epsilon)
-            //{
-            //    Assert.AreEqual(0, 0);
-            //}
-            //else
-            //{
-            //    Assert.AreEqual(0, 1);
-            //}
-            
+        [DataTestMethod]
+        [DataRow(4, 2, 2)]
+        [DataRow(10, 3, 3.33333333)] 
         
+
+        public void DividirsesWithDifrentValue(double left, double right, double expected)
+        {
+            var calculator = new MyCalculator(8);
+            var actual = calculator.Dividirses(left, right);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(3, 2, 6)]
+        [DataRow(-5, 3, -15)]
+
+
+        public void MultiplicacionsWithDifrentValue(double left, double right, double expected)
+        {
+            var calculator = new MyCalculator(8);
+            var actual = calculator.Multiplicacions(left, right);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(16, 4)]
+  
+        public void Sqrt(double value, double expected)
+        {
+            var calculator = new MyCalculator(8);
+            var actual = calculator.Sqrt(value); 
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- MyCalculator.BL/MyCalculator.cs
+++ MyCalculator.BL/MyCalculator.cs
@@ -4,9 +4,33 @@ namespace MyCalculator.BL
 {
     public class MyCalculator
     {
+        public int RoundDigits { get; }
+        public MyCalculator(int roundDigits) // конструктор?
+        // public MyCalculator(int roundDigits = 7) лайфхак от Сереги
+        {
+            RoundDigits = roundDigits;
+        }
+
         public double Summ(double left, double right)
         {
-            return left + right;
+            return Math.Round(left + right, RoundDigits);
+        }
+        public double Minus(double left, double right)
+        {
+            return left - right;
+        }
+        public double Dividirses(double left, double right)
+        {
+            return Math.Round(left / right, RoundDigits);
+        }
+
+        public double Multiplicacions(double left, double right)
+        {
+            return left * right;
+        }
+        public double Sqrt(double value)
+        {
+            return Math.Round( Math.Sqrt(value), RoundDigits);
         }
     }
 }

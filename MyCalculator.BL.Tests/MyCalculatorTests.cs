db0bc5196ab9ed581130139c4f6819247c970c77
Admin <trac-andrej@ya.ru>
2020-03-04 10:01:16 +0300

    Попробовал через [Datarow], вроде работает.

--- MyCalculator.BL.Tests/MyCalculatorTests.cs
+++ MyCalculator.BL.Tests/MyCalculatorTests.cs
@@ -6,9 +6,19 @@ namespace MyCalculator.BL.Tests
     [TestClass]
     public class MyCalculatorTests
     {
-        [TestMethod]
-        public void Summ_10and20_30returned()
+        [DataTestMethod]
+        [DataRow(-1, 1, 0)]
+        [DataRow(10, 11, 21)]
+        [DataRow(11, 1, 12)]
+        [DataRow(10, 1, 11)]
+
+
+        public void SummWithDifrentValue(double left, double right, double expected)
         {
+            var calculator = new MyCalculator();
+            var actual = calculator.Summ(left, right);
+            Assert.AreEqual(expected, actual);
+
             // arrange
             //double left = 10;
             //double right = 20;
